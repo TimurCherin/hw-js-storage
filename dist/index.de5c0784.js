42c39b60499e17ea46d506a15bd0626b0b1c71ea
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2024-01-21 23:42:27 +0200

    d

---
+++ dist/index.de5c0784.js
@@ -0,0 +1,9 @@
+const list = document.querySelector(".todo-list");
+const item = document.querySelectorAll(".todo-item");
+const btn = document.querySelectorAll(".todo-button");
+list.addEventListener("submit", onSubmit);
+function onSubmit(e) {
+    console.log(223);
+}
+
+//# sourceMappingURL=index.de5c0784.js.map
---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import listTemplate from '../templates/template.hbs';
+console.log(listTemplate)
+// const list = document.querySelector(".todo-list")
+// const item = document.querySelectorAll(".todo-item")
+// const btn = document.querySelectorAll(".todo-button")
+// list.addEventListener("submit", onSubmit)
+// function onSubmit(e) {
+//     console.log(223)
+// }

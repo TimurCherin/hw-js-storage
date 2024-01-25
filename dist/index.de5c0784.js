1dc92bc7b78dfcb8947fca80b0b3bcdf0152aa6a
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2024-01-25 22:24:53 +0200

    d

--- dist/index.de5c0784.js
+++ dist/index.de5c0784.js
@@ -1,9 +1,9 @@
-const list = document.querySelector(".todo-list");
-const item = document.querySelectorAll(".todo-item");
-const btn = document.querySelectorAll(".todo-button");
-list.addEventListener("submit", onSubmit);
-function onSubmit(e) {
-    console.log(223);
+// import listTemplate from '../templates/template.hbs';
+function markup(item) {
+    list.innerHTML = listTemplate({
+        item
+    });
+    const list = document.querySelector(".todo-list");
 }
 
 //# sourceMappingURL=index.de5c0784.js.map
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,65 @@
 import listTemplate from '../templates/template.hbs';
-console.log(listTemplate)
-// const list = document.querySelector(".todo-list")
-// const item = document.querySelectorAll(".todo-item")
-// const btn = document.querySelectorAll(".todo-button")
-// list.addEventListener("submit", onSubmit)
-// function onSubmit(e) {
-//     console.log(223)
-// }
+import { nanoid } from 'nanoid';
+const addItem = document.querySelector(".addItem")
+const addItemBtn = document.querySelector(".addItemBtn")
+const submitInput = document.querySelector(".submit-input")
+const submitForm = document.querySelector(".submit-form")
+const submitBtn = document.querySelector(".submit-btn")
+const check = prompt("Введіть ваш минулий логін")
+alert("Щоб перевірити чи ввели ви правильно логін напишіть його повторно в інпут")
+function markup(taskList) {
+    const list = document.querySelector(".todo-list")
+    list.innerHTML = listTemplate({ taskList });
+    list.addEventListener("click", onEdit)
+}
+if (localStorage.getItem("taskList")) {
+    const taskList = JSON.parse(localStorage.getItem("taskList"))
+    markup(taskList)
+}
+addItemBtn.addEventListener("click", onAddTask)
+function onAddTask(e) {
+    if (!localStorage.getItem("taskList")) {
+        localStorage.setItem("taskList", "[]")
+    }
+    const taskList = JSON.parse(localStorage.getItem("taskList"))
+    const currentTask = addItem.value
+    if (!currentTask) {
+        return  
+    }
+    const newTask = {
+        id: nanoid(),
+        task: currentTask,
+        status: false
+    }
+    taskList.push(newTask)
+    addItem.value = ""
+    markup(taskList)
+    localStorage.setItem("taskList", JSON.stringify(taskList))
+}
+
+function onEdit(e) {
+    if (e.target.nodeName === "BUTTON" && e.target.hasAttribute("data-id")) {
+        const editId = e.target.dataset.id
+        const taskList = JSON.parse(localStorage.getItem("taskList"))
+        const editIndex = taskList.map(ts => ts.id).indexOf(editId)
+        if (taskList[editIndex].status) {
+             taskList[editIndex].status = false;
+        } else {
+            taskList[editIndex].status = true;
+        }
+        markup(taskList)
+        localStorage.setItem("taskList", JSON.stringify(taskList))
+    }
+}
+submitBtn.addEventListener("click", onSubmit)
+function onSubmit(e) {
+    e.preventDefault()
+    const info = JSON.stringify(submitInput.value)
+    const currentInfo = localStorage.getItem("info", info)
+    localStorage.setItem("info", info)
+    if (`"${check}"` == currentInfo) {
+        alert("Ви правильно ввели минулий логін")
+    } else {
+        alert("Ви не правильно ввели минулий логін")
+    }
+}
